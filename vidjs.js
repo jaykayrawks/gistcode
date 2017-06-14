06018c67774ba440220f0f7a79a02a646f6aad72
Jatin Khurana <Jatin Khurana@MCKINSEY.COM>
2017-06-14 12:15:55 +0530

    added vidjs

---
+++ vidjs.js
@@ -0,0 +1,7 @@
+videojs.plugin('pluginLocalize', function() {
+  videojs.addLanguage('de', {
+      "play Video": "dont",
+      "hello":"voila",
+      "Goodbye": "Auf Wiedersehen",
+      "Auf Wiedersehen":"goodbye"
+  });
